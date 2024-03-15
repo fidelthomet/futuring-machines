9b708f959b7ac4f2f95d61e992c8ab5175eefbc7
fidelthomet <git@ft0.ch>
2024-03-15 16:48:34 +0100

    add diverge tone command

--- src/assets/prompts/index.js
+++ src/assets/prompts/index.js
@@ -2,5 +2,6 @@ import condense from './condense'
 import pushForward from './pushForward'
 import imagine from './imagine'
 import diverge from './diverge'
+import diverge2 from './diverge2'
 
-export default [condense, pushForward, diverge, imagine]
+export default [condense, pushForward, diverge, imagine, diverge2]
