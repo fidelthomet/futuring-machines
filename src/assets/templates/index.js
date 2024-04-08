9c96a530fbfe54c5332405dcfdab3be7a20ac499
Jordi Tost <tost.jordi@gmail.com>
2024-04-08 10:18:01 +0200

    Climate + Design templates updated.

--- src/assets/templates/index.js
+++ src/assets/templates/index.js
@@ -1,8 +1,6 @@
 import homes from './homesForLife'
 import homesStart from './homesForLifeStart'
-import design from './design'
 import designMix from './design-mix'
 import climate from './climate'
-import empty from './empty'
 
-export default [homes, homesStart, climate, design, designMix, empty]
+export default [homes, homesStart, climate, designMix]
