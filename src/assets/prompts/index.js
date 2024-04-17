773b248b0c86dabdba74fd6b680ad6c5a51542ff
Jordi Tost <tost.jordi@gmail.com>
2024-04-17 22:07:07 +0200

    Condense mode added.

--- src/assets/prompts/index.js
+++ src/assets/prompts/index.js
@@ -4,10 +4,11 @@ import divergeTone from './divergeTone'
 import perspective from './perspective'
 import question from './question'
 import wildcards from './wildcards'
+import condense from './condense'
 import elaborate from './elaborate'
 import innerMonologue from './innerMonologue'
 import innerMonologueRewrite from './innerMonologueRewrite'
 import pushForwardSelection from './pushForwardSelection'
 
-export default [continueWriting, divergeSTEEP, divergeTone, perspective, question, wildcards,   // New Line
-                elaborate, innerMonologue, innerMonologueRewrite, pushForwardSelection]         // Selection
+export default [continueWriting, divergeSTEEP, divergeTone, perspective, question, wildcards,       // New Line
+                condense, elaborate, innerMonologue, innerMonologueRewrite, pushForwardSelection]   // Selection
