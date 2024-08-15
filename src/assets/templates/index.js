4246e37c9ae4c3f3e7c3fec1658432ee31b340c7
fidelthomet <git@ft0.ch>
2024-08-15 14:52:43 +0200

    #23 harmonise template names

--- src/assets/templates/index.js
+++ src/assets/templates/index.js
@@ -1,19 +1,19 @@
-import climateFutures from './climateFutures'
-import designCUI from './designCui'
-import experientialFutures from './experientialFutures'
-import experientialFuturesDE from './experientialFutures-de'
+import climateFutures from './climateFutures.json'
+import futuresOfDesign from './futuresOfDesign.json'
 import homesForLife from './homesForLife'
 import localCommunity from './localCommunity'
 import nightOfWriting from './nightOfWriting'
 import queerFutures from './queerFutures'
+import thingFromTheFuture from './thingFromTheFuture.json'
+import thingFromTheFutureDE from './thingFromTheFuture_de.json'
 
 export default [
-  experientialFutures,
-  experientialFuturesDE,
+  thingFromTheFuture,
+  thingFromTheFutureDE,
   localCommunity,
   homesForLife,
   climateFutures,
   queerFutures,
   nightOfWriting,
-  designCUI
+  futuresOfDesign
 ]
