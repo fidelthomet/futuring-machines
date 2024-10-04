bbf16dce35c65911e15cbfd897b0c1ee5ca9a457
Jordi Tost <tost.jordi@gmail.com>
2024-10-04 11:01:48 +0200

    Descriptions to Settings page added.

--- src/components/TheSettings.vue
+++ src/components/TheSettings.vue
@@ -14,19 +14,22 @@ const url = ref(import.meta.env.VITE_STORIES_STATIC_URL)
   <div class="settings">
     <h1>Settings</h1>
     <h2>Language</h2>
+    <p>Set the language for the stories.</p>
     <InputSegment :options="settingStore.langOptions" v-model="settingStore.lang" name="lang" />
     <h2>Stories</h2>
     <ButtonDefault primary class="delete" @click="storyStore.deleteStory(null, true)"
       >Delete all local stories</ButtonDefault
     >
     <br /><br />
-    <span>story import</span><br /><input type="text" v-model="url" /><ButtonDefault
+    <span>Story import</span><br /><input type="text" v-model="url" /><ButtonDefault
       primary
       @click="storyStore.importStories(url)"
       >Import</ButtonDefault
     >
 
-    <h2>export</h2>
+    <h2>Export</h2>
+    <p>This setting defines the behavior of the upload/download button in the footer.</p>
+    <p>By default it uploads only, but can also download or both. The icon adapts accordingly.</p>
     <InputSegment :options="settingStore.shareOptions" v-model="settingStore.share" name="export" />
   </div>
 </template>
@@ -36,6 +39,10 @@ const url = ref(import.meta.env.VITE_STORIES_STATIC_URL)
   margin-top: calc(var(--spacing) * 4);
   .delete {
     color: var(--color-user);
+  }
+
+  h2 {
+    margin-top: 30px;
   }
 }
 </style>
